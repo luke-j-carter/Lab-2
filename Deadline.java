4d87eea5e2e3addc21a712bb6c1000be502b231a
luke-j-carter <123020722+luke-j-carter@users.noreply.github.com>
2024-09-25 14:54:35 -0500

    Update Deadline.java

--- Deadline.java
+++ Deadline.java
@@ -3,22 +3,23 @@ import java.time.LocalDateTime;
 public class Deadline extends Event implements Completable {
     private boolean complete;
 
+    //constructor
     public Deadline(String name, LocalDateTime deadline) {
         super(name, deadline);
         this.complete = false;
     }
 
-
+    //gets the name of the deadline
     public String getName() {
         return name;
     }
 
-
+    //sets the deadline to complete
     public void complete() {
         this.complete = true;
     }
 
-
+    //gets the results of complete
     public boolean isComplete() {
         return complete;
     }
